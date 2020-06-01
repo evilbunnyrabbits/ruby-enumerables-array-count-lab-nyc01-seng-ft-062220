4c31d7e7175487e14004f854d673974dfa48047a
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-06-01 12:10:05 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,18 +1,20 @@
 def count_strings(array)
-  # Return the total number of strings in the provided array using the count enumerable
   count = 0
   array.count do |item|
     if item.class == String
       count += 1
+    end
   end
   return count
 end
 
+
 def count_empty_strings(array)
-  # Return the total number of EMPTY strings in the provided array using the count enumerable
-  array.count do |item|
-    count = 0
-    if item == ''
-      count += 1
+  count = 0
+    array.count do |item|
+      if item == ""
+        count += 1
+      end
+    end
     return count
 end
